20e9b57feb68cbabcee076f113dcb41430d9cc38
agumusay <agumusay@protonmail.com>
2020-06-24 12:33:38 +0200

    initial setup

---
+++ App.js
@@ -0,0 +1,37 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import useCachedResources from './hooks/useCachedResources';
+import BottomTabNavigator from './navigation/BottomTabNavigator';
+import LinkingConfiguration from './navigation/LinkingConfiguration';
+
+const Stack = createStackNavigator();
+
+export default function App(props) {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <View style={styles.container}>
+        <NavigationContainer linking={LinkingConfiguration}>
+          <Stack.Navigator>
+            <Stack.Screen name="Root" component={BottomTabNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Pflanzy-mobile-app
 
 Find relevant information on how to grow room plants correctly or let others benefit from your experience - All in one place
+
+Hello. I´m Patrick!
