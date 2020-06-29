74360970190d972dd767119d7aaf95cf8326104d
patrickthumeyer <p.thumeyer@gmail.com>
2020-06-29 16:12:15 +0200

    create plant-link-component in components folder

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PlantLink from '../components/plant-link-component'
 
 const SearchScreen = () => {
   return (
     <View style={styles.container}>
       <Text> Search Screen </Text>
+      <PlantLink/>
     </View>
   );
 };
