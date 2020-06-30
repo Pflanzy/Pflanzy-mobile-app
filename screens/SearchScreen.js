a236cce00729288d647e6b3fcbbbe642bf50ffb7
patrickthumeyer <p.thumeyer@gmail.com>
2020-06-30 15:00:10 +0200

    add card component, Link element missing

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ReminderCard from '../components/reminder-card-component'
 
 const SearchScreen = () => {
   return (
     <View style={styles.container}>
       <Text> Search Screen </Text>
+      <ReminderCard/>
+      <ReminderCard/>
+      <ReminderCard/>
     </View>
   );
 };
