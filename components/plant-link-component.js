4aa3f2f2ccee1a03ca6340875c40aedd6c136a78
patrickthumeyer <p.thumeyer@gmail.com>
2020-06-30 10:25:33 +0200

    fix issue with emulator and minor bugs in plant-link-component

--- components/plant-link-component.js
+++ components/plant-link-component.js
@@ -4,7 +4,7 @@ import { Link } from '@react-navigation/native';
 
 const PlantLink = props => {
     return (
-        <Link to="individual plant page path">
+        <Link to="/individual-plant-page-path">
             <View style={styles.wrapper}>
                 <Image style={styles.image} source={{uri: 'https://picsum.photos/seed/picsum/200/200'}}></Image>
             </View>
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     image: {
         width: 80,
         height: 80,
-        borderRadius: '50%',
+        borderRadius: 50,
         margin: 15,
     }
 })
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import PlantLink from '../components/plant-link-component'
 
 const SearchScreen = () => {
   return (
     <View style={styles.container}>
       <Text> Search Screen </Text>
-      <PlantLink/>
     </View>
   );
 };
 
 export default SearchScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container:{}
+});
