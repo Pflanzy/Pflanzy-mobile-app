d2c7f0029e4d1285ab1ce16777a32f835313e5d3
eminaydin <eminaydin93@yahoo.com>
2020-07-01 12:24:31 +0200

    DailyTask component created

--- components/DailyTask.js
+++ components/DailyTask.js
@@ -1,72 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View,Image,ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons"
 
-
-const DailyTask = () => {
+const DailyTask = props => {
     const styles = StyleSheet.create({
-        dailyTaskContainer:{
-            display:"flex",
-            height:"100%"
+        dailyTaskContainer: {
+            display: "flex",
+            height: "100%"
         },
         image: {
-            width:"100%",
-            height:300
+            width: "100%",
+            height: 300
         },
         detailCard: {
-         alignItems:"center",
-         borderColor:"black",
-         borderWidth:1,
-         margin:10
+            alignItems: "center",
+            margin: 10
         },
         taskHeader: {
-            borderColor:"black",
-            borderWidth:1,
-            width:"50%",
-            alignItems:"center",
-            margin:10
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: "100%",
+            alignItems: "center",
+            padding: 10
         },
-        taskBody: { 
-            borderRadius:10,
-            padding:10,
-            shadowColor:"black",
-            shadowOffset:{width:2, height:2},
-            shadowRadius:2,
-            shadowOpacity:0.26,
-            backgroundColor:"white"
+        taskName: {
+            justifyContent: "space-evenly",
+        },
+        iconsNeeded: {
+            flexDirection: "row",
+            width: "100%"
+        },
+        taskBody: {
+            borderRadius: 10,
+            padding: 10,
         },
         taskExtra: {
-            borderColor:"black",
-            borderWidth:1,
-            margin:10
+            margin: 10
         }
     })
     return (
-        <ScrollView style={styles.dailyTaskContainer}> 
-      <View>
-          <Image 
-          style={styles.image}
-          source={{
-          uri: 'https://picsum.photos/200',
-        }} />
-        <View style={styles.detailCard}>
-<View style={styles.taskHeader}> 
-        <Text>Task</Text>
+        <ScrollView style={styles.dailyTaskContainer}>
+            <View>
+                <Image
+                    style={styles.image}
+                    source={{
+                        uri: 'https://picsum.photos/200',
+                    }} />
+                <View style={styles.detailCard}>
+                    <View style={styles.taskHeader}>
+                        <View style={styles.taskName}>
+                            <Text style={{ margin: 5 }}>Today's task</Text>
+                            <Icon name="ios-water" size={30} color="green" style={{ textAlign: "center" }} />
+                        </View>
 
-</View>
-       <View style={styles.taskBody}>
-           
-        <Text>Tasks description</Text>
-        <Text>Suggested Time</Text>
-        <Text>Tools Needed</Text>
-           </View>
-           <View style={styles.taskExtra}>
+                        <View style={styles.taskBody}>
+                            <Text>Tasks description</Text>
+                            <Text>5 minutes</Text>
+                            <View style={styles.iconsNeeded}>
 
-        <Text>Extra Information Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro laboriosam ad officiis quaerat mollitia ullam quidem cumque odit corrupti odio, quibusdam itaque, quo sit recusandae quod a aperiam natus ratione, quisquam animi beatae dolor expedita aliquid veniam? Necessitatibus laudantium et autem incidunt suscipit. Laborum ipsa voluptatibus inventore nisi tempore fugit?</Text>
-               </View> 
-        </View>
+                                <MaterialIcon name="cup-water" size={30} color="green" />
+                                <MaterialIcon name="flower" size={30} color="green" />
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.taskExtra}>
 
-      </View>
-      </ScrollView>
+                        <Text>Extra Information Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro laboriosam ad officiis quaerat mollitia ullam quidem cumque odit corrupti odio, quibusdam itaque, quo sit recusandae quod a aperiam natus ratione, quisquam animi beatae dolor expedita aliquid veniam? Necessitatibus laudantium et autem incidunt suscipit. Laborum ipsa voluptatibus inventore nisi tempore fugit?</Text>
+                    </View>
+                </View>
+
+            </View>
+        </ScrollView>
     );
 }
 
