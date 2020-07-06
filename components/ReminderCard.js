371f9983244cac3e013088c7c760514dc75b409f
patrickthumeyer <p.thumeyer@gmail.com>
2020-07-06 21:32:25 +0200

    remove empty space in ReminderCard

--- components/ReminderCard.js
+++ components/ReminderCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-
 const ReminderCard = (props) => {
   const navigation = useNavigation();
   return (
