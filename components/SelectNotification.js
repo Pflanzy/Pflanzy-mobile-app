7eeab4b00c41cff4f8d92400ae97414ff43b2196
Julianb18 <julian141718@gmail.com>
2020-07-12 10:20:01 +0200

    stash

--- components/SelectNotification.js
+++ components/SelectNotification.js
@@ -1,22 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 function SelectNotification() {
   return (
     <View style={styles.mainContainer}>
-      <View>
-        <Text>Sunlight</Text>
-        <Text>Water</Text>
-        <Text>Fertilize</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.notificationImage}
+          source={require('../assets/images/water-lilly.jpg')}
+        />
       </View>
+      <View style={styles.selectorContainer}>
+        <ScrollView horizontal>
+          <Text>Sunlight</Text>
+          <Text>Water</Text>
+          <Text>Fertilize</Text>
+        </ScrollView>
+      </View>
+      <svg height="500" width="500">
+        <polygon points="50,20 30,80 40,60" stroke="cadetblue" strokeWidth="5" />
+      </svg>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
-    borderWidth: 2,
-    height: '50%',
+    borderWidth: 1,
+    height: '60%',
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: '100%',
+    height: '70%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  notificationImage: {
+    height: '100%',
+    width: '50%',
+  },
+  selectorContainer: {
+    // flexDirection: 'row',
+    // justifyContent: 'space-between',
+    // alignItems: 'center',
+    width: '60%',
+    height: '30%',
+    borderWidth: 1,
   },
 });
 
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,20 +1,12 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-// import ShareIcon from '../components/share-app-component';
 import Select from '../components/SelectNotification';
-=======
->>>>>>> master
 
 const SearchScreen = () => {
   return (
     <View style={styles.container}>
       <Text> Search Screen </Text>
-<<<<<<< HEAD
-      {/* <ShareIcon/> */}
       <Select />
-=======
->>>>>>> master
     </View>
   );
 };
