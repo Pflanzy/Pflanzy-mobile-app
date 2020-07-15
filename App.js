346086d7e41093a6b9d4555032aa0554d4eac687
agumusay <agumusay@protonmail.com>
2020-07-15 14:55:44 +0200

    format

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { StyleSheet, View, SafeAreaView, StatusBar} from 'react-native';
-import Colors from './constants/Colors'
+import { StyleSheet, View, SafeAreaView, StatusBar } from 'react-native';
+import Colors from './constants/Colors';
 
 import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
@@ -21,22 +21,26 @@ export default function App(props) {
   //   return null;
   // } else {
   return (
-<React.Fragment>
-    <SafeAreaView style={styles.safeAreaTop}/>
-    <StatusBar barStyle="light-content" />
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerStyle: {backgroundColor: Colors.tintColor}, headerTintColor: Colors.defaultWhite}}>
-          <Stack.Screen name="Root" component={BottomTabNavigator} />
-          <Stack.Screen name="Camera" component={CameraScreen} />
-          <Stack.Screen name="DailyTasks" component={DailyTasksScreen} />
-          <Stack.Screen name="IndividualPlantPage" component={IndividualPageScreen} />
-          <Stack.Screen name="IndividualArticle" component={IndividualArticle} />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar style="auto" />
-    </View>
-</React.Fragment>
+    <>
+      <SafeAreaView style={styles.safeAreaTop} />
+      <StatusBar barStyle="light-content" />
+      <View style={styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: { backgroundColor: Colors.tintColor },
+              headerTintColor: Colors.defaultWhite,
+            }}>
+            <Stack.Screen name="Root" component={BottomTabNavigator} />
+            <Stack.Screen name="Camera" component={CameraScreen} />
+            <Stack.Screen name="DailyTasks" component={DailyTasksScreen} />
+            <Stack.Screen name="IndividualPlantPage" component={IndividualPageScreen} />
+            <Stack.Screen name="IndividualArticle" component={IndividualArticle} />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 // }
