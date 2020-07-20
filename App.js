53f5e217c4468d2c7ab3708c91a95f4c6e6183b8
Ali San <53179212+agumusay@users.noreply.github.com>
2020-07-20 10:23:13 +0200

    Update App.js

--- App.js
+++ App.js
@@ -57,10 +57,7 @@ export default function App(props) {
             }}>
             <Stack.Screen name="Root" component={BottomTabNavigator} />
             <Stack.Screen name="Camera" component={CameraScreen} />
-<<<<<<< HEAD
             <Stack.Screen name="MyGardenPlant" component={MyPlantScreen} />
-=======
->>>>>>> b39b904713a722de005534dc5249323dd151c199
             <Stack.Screen name="IndividualPlantPage" component={IndividualPageScreen} />
             <Stack.Screen name="IndividualArticle" component={IndividualArticle} />
           </Stack.Navigator>
