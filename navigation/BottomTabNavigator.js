32fc061ed50b20ae53f310490f2397f0878b6e88
patrickthumeyer <p.thumeyer@gmail.com>
2020-07-20 18:20:27 +0200

    change Explore Page to Explore

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -58,7 +58,7 @@ export default function BottomTabNavigator({ navigation, route }) {
         name="Explore"
         component={ExploreScreen}
         options={{
-          title: 'Explore Page',
+          title: 'Explore',
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="ios-book"/>,
         }}
       />
