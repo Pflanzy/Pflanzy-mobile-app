9524202414fd240930e843438a85245841472441
motoki <kmomoky@hotmail.com>
2020-07-22 13:41:52 +0200

    Import & apply dateInfo from DateTimePicker to notification

--- components/SetReminderNotification.js
+++ components/SetReminderNotification.js
@@ -2,7 +2,7 @@ import * as Notifications from 'expo-notifications';
 
 export default function SetReminderNotification(dateInfo) {
   console.warn('A data sent from DateTimePicker!', dateInfo);
-  console.warn('A data formatted into milliseconds', Date.parse(dateInfo));
+
   Notifications.setNotificationHandler({
     handleNotification: async () => ({
       shouldShowAlert: true,
@@ -11,13 +11,22 @@ export default function SetReminderNotification(dateInfo) {
     }),
   });
 
+  // const trigger = new Date(Date.now() + 60 * 60 * 1000);
+  // trigger.setMinutes(0);
+  // trigger.setSeconds(0);
+  // trigger.seconds = 30;
+  console.warn('A data formatted into milliseconds', Date.parse(dateInfo));
+  const dataIntoMillisec = Date.parse(dateInfo);
+  const dateInSeconds = dataIntoMillisec / 1000;
+
   Notifications.scheduleNotificationAsync({
     content: {
       title: 'Water time!',
       body: "I'm so thirsty🌵...",
     },
+    // trigger,
     trigger: {
-      seconds: 30,
+      seconds: dateInSeconds + 30,
       // repeats: true,
     },
   });
