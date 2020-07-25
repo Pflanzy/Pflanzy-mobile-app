bf48b6ae4b46af71ad23523dd832a24e33d1a790
agumusay <agumusay@protonmail.com>
2020-07-25 02:38:25 +0200

    update & combine user in redux from firebase changes

--- firebase/index.js
+++ firebase/index.js
@@ -2,7 +2,7 @@ import firebase from './config';
 
 export default firebase;
 
-export const signIn = () => {
+export const signInAnonymous = () => {
   return firebase.auth().signInAnonymously();
 };
 
@@ -12,4 +12,16 @@ export const signOut = () => {
 
 export const onAuthStateChanged = (cb) => {
   firebase.auth().onAuthStateChanged(cb);
+};
+
+export const updateUser = (id) => {
+  return (dispatch) => {
+    firebase
+      .firestore()
+      .collection('plants')
+      .doc(id)
+      .onSnapshot((snapshot) => {
+        dispatch({ type: 'UPDATE_USER', payload: snapshot.data() });
+      });
+  };
 };
