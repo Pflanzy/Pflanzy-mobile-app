bae9fb07139053433b43a59c6eba6612190f70f9
agumusay <agumusay@protonmail.com>
2020-07-25 02:36:35 +0200

    update user redux action

--- store/userReducer.js
+++ store/userReducer.js
@@ -1,10 +1,13 @@
 const initialState = {
   plants: [],
 };
+
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_USER':
       return { ...action.payload.user };
+    case 'UPDATE_USER':
+      return { ...state, plants: action.payload.plants };
     case 'REMOVE_USER':
       break;
     case 'ADD_PLANT':
