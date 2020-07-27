96c7414381f62ab344a0992014fee3c89eb14e4c
eminaydin <eminaydin93@yahoo.com>
2020-07-27 09:23:17 +0200

    create new reducer

--- store/index.js
+++ store/index.js
@@ -1,9 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import userReducer from './userReducer';
-
+import modalReducer from "./modalReducer"
 const store = createStore(
-  userReducer,
+combineReducers({userReducer,modalReducer}),
   applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
