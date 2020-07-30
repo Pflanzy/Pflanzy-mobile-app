4f6833318801d1b21b9db01eca14d03b778f6bf3
eminaydin <eminaydin93@yahoo.com>
2020-07-30 15:50:12 +0200

    Merge branch 'emin/color-theme' of github.com:Pflanzy/Pflanzy-mobile-app into emin/color-theme

--- screens/IndividualPlantScreen.js
+++ screens/IndividualPlantScreen.js
@@ -8,16 +8,7 @@ import {
 import React, { useRef, useState } from 'react';
 import BottomSheet from 'reanimated-bottom-sheet';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  StyleSheet,
-  TouchableWithoutFeedback,
-  Image,
-  Text,
-  View,
-  ImageBackground,
-  Modal,
-  Button,
-} from 'react-native';
+import { StyleSheet, TouchableWithoutFeedback, Image, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Transitioning, Transition } from 'react-native-reanimated';
@@ -97,7 +88,7 @@ const IndividualPlantScreen = (navigation) => {
               <NeuMorph>
                 <View style={{ marginBottom: 30 }}>
                   <LinearGradient
-                    colors={['#004e57', '#004e57', '#004e57']}
+                    colors={[Colors.darkGreen, Colors.darkGreen, Colors.darkGreen]}
                     start={[0.0, 0.0]}
                     end={[1.0, 1.0]}
                     style={{
@@ -124,22 +115,22 @@ const IndividualPlantScreen = (navigation) => {
             <Text style={styles.text}>{plant.description}</Text>
             <View style={styles.shortInfoContainer}>
               <View style={styles.shortInfoElement}>
-                <Entypo style={styles.shortInfoIcon} name="globe" size={80} color={Colors.darkGreen} />
+                <Entypo style={styles.shortInfoIcon} name="globe" size={80} color="#416241" />
                 <Text style={styles.shortInfoHeadline}>Origin:</Text>
                 <Text style={styles.shortInfoText}>{plant.origin}</Text>
               </View>
               <View style={styles.shortInfoElement}>
-                <Entypo style={styles.shortInfoIcon} name="price-tag" size={80} color={Colors.darkGreen} />
+                <Entypo style={styles.shortInfoIcon} name="price-tag" size={80} color="#416241" />
                 <Text style={styles.shortInfoHeadline}>Family:</Text>
-                    <Text style={styles.shortInfoText}>{plant.familyName}</Text>
+                <Text style={styles.shortInfoText}>{plant.familyName}</Text>
               </View>
               <View style={styles.shortInfoElement}>
-                <Entypo style={styles.shortInfoIcon} name="tree" size={80} color={Colors.darkGreen} />
+                <Entypo style={styles.shortInfoIcon} name="tree" size={80} color="#416241" />
                 <Text style={styles.shortInfoHeadline}>Growth:</Text>
                 <Text style={styles.shortInfoText}>{plant.maxGrowth}</Text>
               </View>
               <View style={styles.shortInfoElement}>
-                <AntDesign style={styles.shortInfoIcon} name="warning" size={80} color={Colors.darkGreen} />
+                <AntDesign style={styles.shortInfoIcon} name="warning" size={80} color="#416241" />
                 <Text style={styles.shortInfoHeadline}>Poisonous:</Text>
                 <Text style={styles.shortInfoText}>{plant.poisonousForPets}</Text>
               </View>
@@ -288,6 +279,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: ' 100%',
   },
+  btnReminder: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97BF7A',
+    width: 240,
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: '#79C3CA',
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 18,
+    paddingLeft: 6,
+  },
   text: {
     color: Colors.defaultWhite,
     lineHeight: 28,
@@ -306,7 +314,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     width: '44%',
-    backgroundColor: "#97BF7A",
+    backgroundColor: Colors.darkGreen,
     height: 120,
     borderRadius: 10,
     margin: 9,
@@ -334,7 +342,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: "#97BF7A",
+    backgroundColor: Colors.darkGreen,
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
