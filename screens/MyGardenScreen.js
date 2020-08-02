9d334382dd6fee50ae8963f7c08a32f1300d6fcb
eminaydin <eminaydin93@yahoo.com>
2020-08-02 15:45:20 +0200

    Delete flatlist on mygarden page

--- screens/MyGardenScreen.js
+++ screens/MyGardenScreen.js
@@ -1,46 +1,48 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, ScrollView, Text, Image } from 'react-native';
 import { useSelector } from 'react-redux';
 import BasicCard from '../components/BasicCard';
-import { OptimizedFlatList } from 'react-native-optimized-flatlist';
 import Colors from '../constants/Colors';
 
 const MyGardenScreen = () => {
   const plants = useSelector((state) => state.userReducer.plants);
-  const renderItem = ({item}) => {
-    return <BasicCard plant={item}/>
-  }
   return (
-    <View style={styles.container}>
+  <View style={{backgroundColor:Colors.lightPink, minHeight:"100%"}}>
+    <ScrollView contentContainerStyle={styles.wrapper}>
       {plants?.length > 0 ? (
-         <OptimizedFlatList data={plants} renderItem={renderItem} />
+        <View style={styles.container}>
+          {plants.map((plant) => {
+              return <BasicCard plant={plant} key={plant.id} />;
+            })}
+        </View>
       ) : (
         <View style={styles.plantsIconWrapper}>
-          <Image source={{uri:"https://media1.tenor.com/images/22e52165d824572fa4ece1ee968aa6f6/tenor.gif?itemid=16288922"}} style={styles.plantsIcon} />
+          <Image source={{uri:"https://media1.tenor.com/images/22e52165d824572fa4ece1ee968aa6f6/tenor.gif?itemid=16288922"}} style={styles.plantsIconIcon} />
           <Text style={styles.errorMsg}>There are no plants in your garden yet.</Text>
         </View>
       )}
+    </ScrollView>
     </View>
-
   );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    marginTop: 10,
+    backgroundColor:Colors.lightPink
+  },
   container: {
-    justifyContent:"center",
-    alignItems:"center",
-    backgroundColor:Colors.lightPink,
-    minHeight:"100%",
-    minWidth:400
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   plantsIconWrapper: {
     justifyContent: 'center',
     height: 550,
     alignItems: 'center',
   },
-  plantsIcon: {
-    height: 250,
-    width: 250,
+  plantsIconIcon: {
+    height: 350,
+    width: 350,
     marginBottom: 10,
   },
   errorMsg: {
