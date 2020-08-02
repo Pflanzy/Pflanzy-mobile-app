a390dbd9798168cd78a1b04eed26b2f62a10b7ff
eminaydin <eminaydin93@yahoo.com>
2020-08-02 14:10:36 +0200

    Import flatlist on Explore page

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,31 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
 import ExploreCard from '../components/ExploreCard';
 import exploreData from '../data/browse.json';
+import { OptimizedFlatList } from 'react-native-optimized-flatlist';
 
 const ExploreScreen = () => {
-  const articleList = () => {
-    return exploreData.map((article) => {
-      return (
-        <View key={article.id}>
-          <ExploreCard article={article} />
-        </View>
-      );
-    });
+  const renderItem = ({ item }) => {
+    return <ExploreCard article={item} />;
   };
-
   return (
-    <View style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollContainer}>{articleList()}</ScrollView>
-    </View>
+      <OptimizedFlatList
+        data={exploreData}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
   );
 };
-
-const styles = StyleSheet.create({
-  scrollContainer: {
-    paddingVertical: 12,
-  },
-});
-
 export default ExploreScreen;
