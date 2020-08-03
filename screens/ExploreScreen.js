716edc735b881b0bceef55e47271bb3a5feaecef
patrickthumeyer <p.thumeyer@gmail.com>
2020-08-03 08:50:22 +0200

    add margin to Top and Bottom on FlatLists

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import ExploreCard from '../components/ExploreCard';
-import exploreData from '../data/browse.json';
+import { StyleSheet } from 'react-native';
 import { OptimizedFlatList } from 'react-native-optimized-flatlist';
+import exploreData from '../data/browse.json';
+import ExploreCard from '../components/ExploreCard';
 
 const ExploreScreen = () => {
   const renderItem = ({ item }) => {
     return <ExploreCard article={item} />;
   };
   return (
-      <OptimizedFlatList
-        data={exploreData}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
+    <OptimizedFlatList
+      data={exploreData}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id.toString()}
+      style={styles.wrapper}
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    marginVertical: 10,
+  },
+});
+
 export default ExploreScreen;
